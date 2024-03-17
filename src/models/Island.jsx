09c69f5e60ef144efa06450b008e52e85f034cbc
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-17 00:33:35 -0400

    Make island visible on canvas

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -6,62 +6,46 @@ Source: https://sketchfab.com/3d-models/foxs-islands-163b68e09fcc47618450150be77
 Title: Fox's islands
 */
 
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import islandScene from "../assets-2/3d/island.glb";
 import { a } from "@react-spring/three";
 
-export function Model(props) {
-  const { nodes, materials } = useGLTF("/island.glb");
+const Island = (props) => {
+  const islandRef = useRef();
+
+  const { nodes, materials } = useGLTF(islandScene);
+
   return (
-    <a.group {...props} dispose={null}>
+    <a.group ref={islandRef} {...props}>
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface948_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface949_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.pCube11_rocks1_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
+      <mesh geometry={nodes.pCube11_rocks1_0.geometry} material={materials.PaletteMaterial001} />
     </a.group>
   );
-}
-
-useGLTF.preload("/island.glb");
+};
 
 export default Island;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <spotLight />
+          <Island scale={islandScale} position={islandPosition} rotation={islandRotation} />
+        </Suspense>
       </Canvas>
     </section>
   );
