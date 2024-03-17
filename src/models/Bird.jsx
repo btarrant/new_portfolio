ccab760362fa0e0d9a83fa59795b7f09bcbf057a
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-17 16:37:26 -0400

    Add comments

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -13,18 +13,25 @@ const Bird = () => {
   }, []);
 
   useFrame(({ clock, camera }) => {
+    // Update Y position to simulate a sine wave for bird flight
     birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
 
+    // Check if bird reaches a certain endpoint relative to camera
     if (birdRef.current.position.x > camera.position.x + 10) {
+      // Change direction to backwards and rotate the bird 180 degrees on the y-axis
       birdRef.current.rotation.y = Math.PI;
     } else if (birdRef.current.position.x < camera.position.x - 10) {
+      // Change direction to forward and reset bird rotation
       birdRef.current.rotation.y = 0;
     }
 
+    // Update X and Z positions based on direction
     if (birdRef.current.rotation.y === 0) {
+      // Forward
       birdRef.current.position.x += 0.01;
       birdRef.current.position.z -= 0.01;
     } else {
+      // Backwards
       birdRef.current.position.x -= 0.01;
       birdRef.current.position.z += 0.01;
     }
