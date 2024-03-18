627f683a0ba4c5c940246d382f8b65ada0dbe567
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-18 00:44:12 -0400

    Refactor

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       {alert.show && <Alert {...alert} />}
-      <Alert {...alert} />
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in touch!</h1>
         <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
