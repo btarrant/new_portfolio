57d3201c9916dcd625e575c01369ecb169ee8d3f
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-18 02:16:21 -0400

    Add and organize logos for skills section

--- src/assets-2/icons/index.js
+++ src/assets-2/icons/index.js
@@ -1,59 +1,87 @@
-import css from './css.svg'
-import express from './express.svg'
-import git from './git.svg'
-import github from './github.svg'
-import html from './html.svg'
-import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
-import nodejs from './nodejs.svg'
-import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
-import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
-import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
-import car from './car.svg'
-import estate from './estate.svg'
-import pricewise from './pricewise.svg'
-import snapgram from './snapgram.svg'
-import summiz from './summiz.svg'
-import threads from './threads.svg'
-import arrow from './arrow.svg'
-import contact from './contact.svg'
-import soundon from './soundon.png'
-import soundoff from './soundoff.png'
+import aws from "./aws.svg";
+import css from "./css.svg";
+import docker from "./docker.svg";
+import express from "./express.svg";
+import figma from "./figma.svg";
+import flask from "./flask.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import html from "./html.svg";
+import illustrator from "./illustrator.svg";
+import java from "./java.svg";
+import javascript from "./javascript.svg";
+import jira from "./jira.svg";
+import kotlin from "./kotlin.svg";
+import motion from "./motion.svg";
+import mui from "./mui.svg";
+import mysql from "./mysql.svg";
+import nextjs from "./nextjs.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import redux from "./redux.svg";
+import sass from "./sass.svg";
+import tailwindcss from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import car from "./car.svg";
+import estate from "./estate.svg";
+import pricewise from "./pricewise.svg";
+import photoshop from "./photoshop.svg";
+import postman from "./postman.svg";
+import python from "./python.svg";
+import selenium from "./selenium.svg";
+import snapgram from "./snapgram.svg";
+import summiz from "./summiz.svg";
+import threads from "./threads.svg";
+import threejs from "./threejs.svg";
+import arrow from "./arrow.svg";
+import contact from "./contact.svg";
+import soundon from "./soundon.png";
+import soundoff from "./soundoff.png";
+import playwright from "./playwright.svg";
 
 export {
-    css,
-    express,
-    git,
-    github,
-    html,
-    javascript,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript,
-    linkedin,
-    twitter,
-    car,
-    estate,
-    pricewise,
-    snapgram,
-    summiz,
-    threads,
-    arrow,
-    contact,
-    soundon,
-    soundoff
-}
+  aws,
+  css,
+  docker,
+  express,
+  figma,
+  flask,
+  git,
+  github,
+  html,
+  illustrator,
+  java,
+  javascript,
+  jira,
+  kotlin,
+  motion,
+  mui,
+  mysql,
+  nextjs,
+  nodejs,
+  react,
+  redux,
+  sass,
+  tailwindcss,
+  typescript,
+  linkedin,
+  twitter,
+  car,
+  estate,
+  pricewise,
+  photoshop,
+  postman,
+  python,
+  selenium,
+  snapgram,
+  summiz,
+  threads,
+  threejs,
+  arrow,
+  contact,
+  soundon,
+  soundoff,
+  playwright,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+import { meta, shopify, starbucks, tesla } from "../assets-2/images";
+import {
+  aws,
+  car,
+  contact,
+  css,
+  docker,
+  estate,
+  figma,
+  flask,
+  git,
+  github,
+  html,
+  illustrator,
+  java,
+  javascript,
+  jira,
+  kotlin,
+  linkedin,
+  mui,
+  mysql,
+  nodejs,
+  pricewise,
+  photoshop,
+  postman,
+  python,
+  react,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  threejs,
+  typescript,
+  selenium,
+  playwright,
+} from "../assets-2/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: aws,
+    name: "AWS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: flask,
+    name: "Flask",
+    type: "Backend",
+  },
+  {
+    imageUrl: docker,
+    name: "Docker",
+    type: "Backend",
+  },
+  {
+    imageUrl: figma,
+    name: "Figma",
+    type: "Frontend",
+  },
+  {
+    imageUrl: threejs,
+    name: "Threejs",
+    type: "Frontend",
+  },
+  {
+    imageUrl: illustrator,
+    name: "Illustrator",
+    type: "Frontend",
+  },
+  {
+    imageUrl: photoshop,
+    name: "Photoshop",
+    type: "Frontend",
+  },
+  {
+    imageUrl: java,
+    name: "Java",
+    type: "Frontend",
+  },
+  {
+    imageUrl: jira,
+    name: "Jira",
+    type: "Version Control",
+  },
+  {
+    imageUrl: kotlin,
+    name: "Kotlin",
+    type: "Frontend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: selenium,
+    name: "Selenium",
+    type: "Testing",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mysql,
+    name: "MySQL",
+    type: "Backend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: postman,
+    name: "Postman",
+    type: "Testing",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: python,
+    name: "Python",
+    type: "Backend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: playwright,
+    name: "Playwright",
+    type: "Testing",
+  },
+];
+
+export const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Starbucks",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "March 2020 - April 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Native Developer",
+    company_name: "Tesla",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Jan 2021 - Feb 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Shopify",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Meta",
+    icon: meta,
+    iconBg: "#a2d2ff",
+    date: "Jan 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/YourGitHubUsername",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Amazon Price Tracker",
+    description:
+      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
+    link: "https://github.com/adrianhajdin/pricewise",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Full Stack Threads Clone",
+    description:
+      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+    link: "https://github.com/adrianhajdin/threads",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Car Finding App",
+    description:
+      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
+    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
+  },
+  {
+    iconUrl: snapgram,
+    theme: "btn-back-pink",
+    name: "Full Stack Instagram Clone",
+    description:
+      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+    link: "https://github.com/adrianhajdin/social_media_app",
+  },
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "Real-Estate Application",
+    description:
+      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
+    link: "https://github.com/adrianhajdin/projects_realestate",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "AI Summarizer Application",
+    description:
+      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
+    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+  },
+];
