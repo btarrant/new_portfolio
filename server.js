96fc0e6c7c10d4913d865bba9310954d3000abc6
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-19 00:02:56 -0400

    reformat

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // Handle all other routes by serving the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "vite build", "index.html"));
 });
 
 // Start the server
