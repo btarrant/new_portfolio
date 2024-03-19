20810e0794584b7b8161f1e7a2d6ad218fecac92
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-19 10:34:13 -0400

    Refactor config to serve JS files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, extname } from "path";
 import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,12 +8,21 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-// Serve static files from the build folder
-app.use(express.static(path.join(__dirname, "vite build")));
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, "dist")));
 
 // Handle all other routes by serving the index.html file
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// Set correct MIME type for JavaScript files
+app.use((req, res, next) => {
+  const ext = extname(req.url);
+  if (ext === ".js") {
+    res.type("application/javascript");
+  }
+  next();
 });
 
 // Start the server
