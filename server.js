d358b5862bc051ef0beae38b694fae776a7f039f
Brandon Tarrant <brandontarrant04@gmail.com>
2024-03-19 00:15:36 -0400

    Refactor express server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import path from "path";
+
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Serve static files from the build folder
 app.use(express.static(path.join(__dirname, "build")));
